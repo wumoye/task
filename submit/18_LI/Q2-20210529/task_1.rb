a389fb0ea20af940a870afa3a915e8047cce138c
wumoye <lee.neil60@gmail.com>
2021-05-30 23:53:43 +0900

    Q2 update

--- submit/18_LI/Q2-20210529/task_1.rb
+++ submit/18_LI/Q2-20210529/task_1.rb
@@ -2,11 +2,11 @@ age = ARGV[0].to_i
 
 case age
 
-when 4...10
+when 4..10
   puts "おはよう"
-when 11...18
+when 11..18
   puts "こんにちは"
-when 19...24,1...3
+when 19..24,1..3
   puts "こんばんは"
 end
 
--- submit/18_LI/Q2-20210529/task_2.rb
+++ submit/18_LI/Q2-20210529/task_2.rb
@@ -4,13 +4,13 @@ weight = ARGV[1].to_f
 BMI = (weight / (height*height)).round(1)
 
 case BMI
-when 0..18.5
+when 0...18.5
   puts '低体重'
-when 18.5..30
+when 18.5...30
   puts '普通体重'
-when 30..35
+when 30...35
   puts '肥満（１度）'
-when 35..40
+when 35...40
   puts '肥満（２度）'
 else
   puts '肥満（４度）'
