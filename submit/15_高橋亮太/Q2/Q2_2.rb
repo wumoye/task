675883d8fbee634378ffc750c1031db656de358b
rt <test@example.com>
2021-06-11 00:13:11 +0900

    add Q2

---
+++ submit/15_高橋亮太/Q2/Q2_2.rb
@@ -0,0 +1,17 @@
+bmi = ARGV[0] / ARGV[1] * ARGV[1]
+puts bmi
+
+case 
+when bmi =< 18.5
+    puts "低体重"
+when bmi == (18.5...25)
+    puts "普通体重"
+when bmi == (25...30)
+    puts "肥満(1度)"
+when bmi == (30...35)
+    puts "肥満(2度)"
+when bmi == (35...40)
+    puts "肥満(3度)"
+when bmi >= 40
+    puts "肥満(4度)"
+end
