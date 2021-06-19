f98945a738c351364c35b4576b0a0bdbe017be29
sinhyez <upthe15752@gmail.com>
2021-06-19 21:17:02 +0900

    Submit Q4

---
+++ submit/16_JANG/Q4/Q4_2.rb
@@ -0,0 +1,14 @@
+# num = ARGV[0].to_i
+
+def multi(num)
+  (1..num).each do |i|
+    (1..num).each do |j|
+      z = i * j < 10 ? " 0" : " "
+      size = z + (i * j).to_s
+      print size
+    end
+    print "\n"
+  end
+end
+
+multi(3)
