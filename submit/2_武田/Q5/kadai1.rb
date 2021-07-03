48ac6e776ee4d735129d3cef837dbb8b34ab8cec
take <test@yahoo.co.jp>
2021-07-03 15:25:55 +0900

    kadai totyuu

--- submit/2_武田/Q5/kadai1.rb
+++ submit/2_武田/Q5/kadai1.rb
@@ -1,86 +1,100 @@
 class Product
-    attr_accessor :code #読み込み書き込み
-    attr_accessor :name #読み込み書き込み
-    attr_accessor :price #読み込み書き込み
+    attr_reader :code #読み
+    attr_reader :name #読み
+    attr_reader :price #読み
 
+    @@count = 0
     def initialize(code, name, price)
         @code = code
         @name = name
         @price = price
+        @@count = @@count + 1
     end
-    def debug()
-        puts "code: #{self.code}"
-        puts "name: #{self.name}"
-        puts "price: #{self.price}"
-    end
-    def getcode
-        @code
-    end
-    def getname
-        @name
-    end
-    def getprice
-        @price
-    end
-
-=begin
-    def debug()
-        puts "code : #{self.code}"
-        puts "name : #{self.name}"
-        puts "price : #{self.price}"
-    end
-=end
 end
 
 class Phone < Product
-    attr_accessor :size
-    attr_accessor :num
+    attr_reader :size
+    attr_reader :num
+    @@p_count = 0
+    attr_reader :p_count
     def initialize(code, name, price, size, num)
         super(code, name, price)
         @size = size
-        @number = num
+        @num = num
+        @@p_count = @@p_count + 1
     end
 
-    def getnum
-        @number
+    def print
+        puts "商品コードは#{sprintf("%06d", code)} 価格は#{price}円 電話番号は#{num}"
     end
 
-    def debug
-        puts "code: #{@code}"
-        puts "name: #{@name}"
-        puts "price: #{@price}"
-        puts "size: #{@size}"
-        puts "num: #{@number}"
+    def count()
+       puts "携帯:#{@@p_count}"
     end
+
 end
 
 class Camera < Product
-    attr_accessor :pixel
+    attr_reader :pixel
     def initialize(code, name, price, pixel)
         super(code, name, price)
         @pixel = pixel
     end
 
-    def getpixel
-        @pixel
-    end
-
-    def debug
-        puts "code: #{@code}"
-        puts "name: #{@name}"
-        puts "price: #{@price}"
-        puts "pixel: #{@pixel}"
+    def print
+        puts "商品コードは#{sprintf("%06d", code)} 価格は#{price}円 画素数は#{pixel}万画素"
     end
 end
 
-test = Product.new(10, "test",100)
-#test.debug
+class Audio < Product
+    attr_reader :capa
 
-#phone = Phone.new(code,name,price,size,num)
-phone1 = Phone.new(111,"name",333,444,555)
-#puts phone1.getnum
-#phone1.debug
+    def initialize(code, name, price, capa)
+        super(code, name, price)
+        @capa = capa
+    end
 
-camera1 = Camera.new(111,"name",333,444)
-#puts phone1.getnum
-camera1.debug
+    def print
+        puts "商品コードは#{sprintf("%06d", code)} 価格は#{price}円 容量は#{capa}GB"
+    end
+end
+puts "携帯電話:"
+(1..3).each do |i|
+    code = rand(100000)
+    price = rand(50000..100000)
+    size = rand(3..7)
+    num = rand(1000000000..9999999999)
+    
+    phone = Phone.new(code, "携帯電話", price, size, num)
+    print i , ")"
+    phone.print
+    phone.p_count
+end
+
+
+
+puts "デジカメ:"
+(1..5).each do |i|
+    code = rand(100000)
+    price = rand(50000..100000)
+    pixel = rand(100..3000)
+    
+    camera = Camera.new(code, "デジカメ", price, pixel)
+    print i , ")"
+    camera.print
+end
+
+
+puts "携帯オーディオ:"
+(1..20).each do |i|
+    code = rand(100000)
+    #require 'securerandom'
+    #code = SecureRandom.random_number(n=999999)
+    price = rand(50000..100000)
+    capa = 2 ** rand(1..6)
+    
+    audio = Audio.new(code, "携帯オーディオ", price, capa)
+    print i , ")"
+    audio.print
+end
+
