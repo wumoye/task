fd2df4e6035e018faca0011cad39085150a893aa
sinhyez <upthe15752@gmail.com>
2021-07-10 14:40:02 +0900

    submit Q4

--- submit/16_JANG/Q6/Q6_3.rb
+++ submit/16_JANG/Q6/Q6_3.rb
@@ -1,4 +1,8 @@
 arr = [ 1, 2, 3, 1, 2, 1, 2, 6, 5 , "c", "ruby", "c"]
+newArr = []
 
+arr.each do |i|
+  newArr << i unless newArr.include?(i)
+end
 
-p arr.uniq
+p newArr
--- submit/16_JANG/Q6/Q6_4.rb
+++ submit/16_JANG/Q6/Q6_4.rb
@@ -1,3 +1,20 @@
 arr = [5,6,1,3,4]
 
-p arr.sort
+def sort(arr)
+  arr1 = arr.dup
+  (0...arr1.length()).each do |i|
+    min = arr1[i]
+    swap = i
+    ((i+1)...arr1.length()).each do |j|
+      if min > arr1[j]
+        min = arr1[j]
+        swap = j
+      end
+    end
+      arr1[i], arr1[swap] = min, arr1[i]
+  end
+  return arr1
+end
+
+
+p sort(arr)
