c3b5facb9ab0a31dbdb726256dfe77536f0a70c9
Bon215 <sasakureta02@gmail.com>
2021-07-17 15:32:06 +0900

    0717

--- submit/7_安保/Q7/0717_3.rb
+++ submit/7_安保/Q7/0717_3.rb
@@ -1,3 +1,12 @@
 class Array
+
+  def with_order_print
+  
+	self.each_with_index  do |p,i|
+		puts i + p 
+	end
+  end
+end
+
 sample_array = ["stay","foolish","stay","hungry"]
 sample_array.with_order_print
