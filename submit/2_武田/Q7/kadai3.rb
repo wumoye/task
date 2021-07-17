465358380ef6387526c2a9998f72ec47ff07f5f1
take <test@yahoo.co.jp>
2021-07-17 15:14:02 +0900

    add Q7

---
+++ submit/2_武田/Q7/kadai3.rb
@@ -0,0 +1,10 @@
+class Array
+    def with_order_print()
+        self.each_with_index do |v, i|
+            puts "#{(i + 1)}) #{v}"
+        end
+    end
+end
+
+sample_array = ["stay","foolish","stay","hungry"]
+sample_array.with_order_print
