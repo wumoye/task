1bd0826dd0fdc72bbcf565f4f88e943de91e846c
yuya <white_gyao4@icloud.com>
2021-09-04 15:24:52 +0900

    Q6

---
+++ submit/9_石関/Q6/Q6.rb
@@ -0,0 +1,15 @@
+#1.1〜100までの数値配列aを用意し、３の倍数のみ取得した配列cを作成し値を出力せよ
+
+
+
+
+#3.下記の配列から重複データを排除して出力せよ
+array_sample = [1,2,3,1,2,1,2,6,5,"c","ruby","c"]
+
+array_sample.uniq!
+p array_sample
+
+#4.下記の配列をソートして出力せよ
+array_sample2 = [5,6,1,3,4]
+
+p array_sample2.sort
