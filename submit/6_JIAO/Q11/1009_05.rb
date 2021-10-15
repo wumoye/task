bc699f9d4b291f79961db0743dc68f4cfedd08c9
kyou <test@yahoo.co.jp>
2021-10-15 13:14:16 +0900

    submit q11

---
+++ submit/6_JIAO/Q11/1009_05.rb
@@ -0,0 +1,19 @@
+class Tool
+    def use
+      puts "color:#{@color} tool:#{self.class.to_s}"
+    end
+
+    def initialize(color)
+      @color = color
+    end
+  end
+class Hammer < Tool
+    def initialize(color)
+        super(color)
+    end
+    def use
+        puts "color:#{@color} tool:#{self.class.to_s}"
+        puts "ハンマーを使う"
+    end
+end
+Hammer.new("red").use
